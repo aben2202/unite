655a3c17e1c0583e97d976a8cfedd418084db10c
Andrew Benson <andebenson@gmail.com>
2012-11-11 14:47:51 -0600

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unite::Application.config.secret_token = '66d4d9635edb975bd4b6532faa7830c3914485a0982be5f16256dcb7bcf22e8e5576d138d7ff4d7203be36985358dfd25e382927621281972f18e9734a230b87'
