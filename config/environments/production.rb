b36efe481822cfdc4954fe61311269ee786ffb36
Andrew Benson <andebenson@gmail.com>
2012-11-29 23:43:52 -0600

    trying to fix server email system

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,21 @@ Unite::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Change mail delivery to either :smtp, :sendmail, :file, :test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "andebenson@gmail.com",
+    password: "adb7886m5"
+  }
+
+  # Specify what domain to use for mailer URLs
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
