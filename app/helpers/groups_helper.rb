1320c0b0d69872cce60b13010a6746c59859c48c
Andrew Benson <andebenson@gmail.com>
2012-12-07 12:02:30 -0600

    user groups now show if private or not

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,7 +2,11 @@ module GroupsHelper
 
 	def get_group_row_class(group)
 		if signed_in? && current_user.member?(group)
-			"group-member"
+			if group.open_to_public?
+				"group-member"
+			else
+				"group-member private-group"
+			end
 		end
 	end
 end
